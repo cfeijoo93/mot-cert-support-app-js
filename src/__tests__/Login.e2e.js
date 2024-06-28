db48680cb69a0d4309df2db75fe189489ac6503a
cfeijoo93 <carolina@lodgify.com>
2024-06-28 17:44:23 +0200

    Added in Github Actions and other tests

---
+++ src/__tests__/Login.e2e.js
@@ -0,0 +1,14 @@
+import { expect, browser, $ } from "@wdio/globals";
+
+describe("My login application", () => {
+  it("should login with valid credentials", async () => {
+    await browser.url(`http://localhost:3000/#/login`);
+
+    await $('input[name="email"]').setValue("admin@test.com");
+    await $('input[name="password"]').setValue("password123");
+    await $("button").click();
+
+    const element = await $(".card-title");
+    await expect(element).toHaveText("Projects");
+  });
+});
